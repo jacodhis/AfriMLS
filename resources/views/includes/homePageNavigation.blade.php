8081e6488e95d3634da5626f0e2eca9a6b2bf3dc
Job Nyamolo <jobnyamolo@gmail.com>
2022-04-27 10:44:16 +0300

    fix disapearring menu on hover

--- resources/views/includes/homePageNavigation.blade.php
+++ resources/views/includes/homePageNavigation.blade.php
@@ -1,5 +1,13 @@
 <li><a href="{{ route('landingPage')}}">Home</a></li>
 <li><a href="{{ route('aboutUs')}}">About Us</a></li>
+<li class="has-dropdown">
+	<a href="#" data-toggle="dropdown" class="dropdown-toggle menu-item">Dropable Menu</a>
+	<ul class="dropdown-menu">
+		<li><a href="#" data-toggle="dropdown" class="dropdown-toggle">Filter 1</a></li>
+		<li><a href="#" data-toggle="dropdown" class="dropdown-toggle">Filter 2</a></li>
+		<li><a href="#" data-toggle="dropdown" class="dropdown-toggle">Filter 3</a></li>
+	</ul>
+</li>
 <li><a href="#">To Let</a></li>
 <li><a href="#">For Sale</a></li>
 
